e191c0cb5dc259fba9e2868e4080a3a5dc417f53
Sweeney <JohnS@BlackhawkDC.com>
2022-10-24 14:35:14 -0500

    began adding UDP features., send function this edit.

--- EthTool/GUI.h
+++ EthTool/GUI.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "udp.h"
+#include <msclr\marshal_cppstd.h>
+
 namespace EthTool {
 
 	using namespace System;
@@ -38,7 +41,13 @@ namespace EthTool {
 	protected:
 
 	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::TextBox^  textBoxDummy;
+
+	private: System::Windows::Forms::TextBox^  textBoxDestPort;
+
+	private: System::Windows::Forms::TextBox^  textBoxPayload;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
 
 	protected:
 
@@ -57,40 +66,98 @@ namespace EthTool {
 		{
 			this->textBoxDestAddr = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBoxDummy = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxDestPort = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxPayload = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// textBoxDestAddr
 			// 
-			this->textBoxDestAddr->Location = System::Drawing::Point(60, 41);
+			this->textBoxDestAddr->Location = System::Drawing::Point(143, 22);
 			this->textBoxDestAddr->Name = L"textBoxDestAddr";
 			this->textBoxDestAddr->Size = System::Drawing::Size(100, 20);
 			this->textBoxDestAddr->TabIndex = 0;
 			this->textBoxDestAddr->Text = L"192.168.2.239";
+			this->textBoxDestAddr->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(85, 77);
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(154, 100);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 1;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Send";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &GUI::button1_Click);
 			// 
-			// textBoxDummy
+			// textBoxDestPort
 			// 
-			this->textBoxDummy->Location = System::Drawing::Point(40, 123);
-			this->textBoxDummy->Name = L"textBoxDummy";
-			this->textBoxDummy->Size = System::Drawing::Size(173, 20);
-			this->textBoxDummy->TabIndex = 2;
+			this->textBoxDestPort->Location = System::Drawing::Point(143, 48);
+			this->textBoxDestPort->Name = L"textBoxDestPort";
+			this->textBoxDestPort->Size = System::Drawing::Size(100, 20);
+			this->textBoxDestPort->TabIndex = 3;
+			this->textBoxDestPort->Text = L"666";
+			this->textBoxDestPort->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
+			// textBoxPayload
+			// 
+			this->textBoxPayload->Location = System::Drawing::Point(143, 74);
+			this->textBoxPayload->Name = L"textBoxPayload";
+			this->textBoxPayload->Size = System::Drawing::Size(100, 20);
+			this->textBoxPayload->TabIndex = 4;
+			this->textBoxPayload->Text = L"fucks yeah";
+			this->textBoxPayload->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(24, 22);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(113, 20);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Destination IP:";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(10, 48);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(127, 20);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Destination Port:";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(59, 72);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(78, 20);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Message:";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
 			// 
 			// GUI
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(283, 201);
-			this->Controls->Add(this->textBoxDummy);
+			this->ClientSize = System::Drawing::Size(262, 154);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBoxPayload);
+			this->Controls->Add(this->textBoxDestPort);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBoxDestAddr);
 			this->Name = L"GUI";
@@ -101,8 +168,18 @@ namespace EthTool {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		// IP address
 		String^ destAddr = System::Convert::ToString(textBoxDestAddr->Text);
-		textBoxDummy->Text = System::Convert::ToString(destAddr);
+		std::string ip = msclr::interop::marshal_as<std::string>(destAddr);
+		const char* IP = ip.c_str();
+		// Port
+		int portNum = System::Convert::ToInt16(textBoxDestPort->Text);
+		//int portNum = msclr::interop::marshal_as<int>(destPort);
+		// Payload
+		String^ message = System::Convert::ToString(textBoxPayload->Text);
+		std::string msg = msclr::interop::marshal_as<std::string>(message);
+
+		sendUDP(IP, portNum, msg);
 	}
 	};
 }
--- EthTool/udp.cpp
+++ EthTool/udp.cpp
@@ -1,52 +1,32 @@
 #include "udp.h"
 
-WSADATA WinSockData;
-SOCKET UDPSocketClient;
 
-void openSocket()
+
+void sendUDP(const char* IP, int portNum, std::string msg)
 {
 	//Initiate Winsock dll.
+	WSADATA WinSockData;
+	SOCKET UDPSocketClient;
 	WSAStartup(MAKEWORD(2, 2), &WinSockData);
 	std::cout << "WSAStarup success" << std::endl;
 
 	//Create socket.
 	UDPSocketClient = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	std::cout << "Socket opened." << std::endl;
-};
 
-void sendPackets()
-{
 	//Struct with destination parameters.
 	struct sockaddr_in dest;
 	dest.sin_family = AF_INET;
-	dest.sin_addr.s_addr = inet_addr("192.168.2.239");
-	dest.sin_port = htons(666);
+	dest.sin_addr.s_addr = inet_addr(IP);
+	dest.sin_port = htons(portNum);
 
-	std::string txbuf = "hell yeah fuckin right.";
-	int txbuflen = txbuf.size();
-	const char* txbufptr;
-	txbufptr = txbuf.data();
-
-
-	char rxbuf[512] = { 0 };
-	int rxbuflen = sizeof(rxbuf);
-
-	int txbytes = 0;
-	int rxbytes = 0;
+	int len = msg.size();
+	const char* msgPtr;
+	msgPtr = msg.data();
 
 	// TX
-	sendto(UDPSocketClient, txbufptr, txbuflen, 0, (SOCKADDR *)& dest, sizeof(dest));
+	sendto(UDPSocketClient, msgPtr, len, 0, (SOCKADDR *)& dest, sizeof(dest));
 
-	// RX
-	do {
-		rxbytes = recv(UDPSocketClient, rxbuf, rxbuflen, 0);
-		std::cout << "Payload:" << rxbuf << std::endl;
-		std::cout << "Payload size:" << rxbytes << std::endl;
-	} while (rxbytes > 0);
-};
-
-void closeSocket()
-{
 	//Close socket.
 	closesocket(UDPSocketClient);
 	std::cout << "Socket closed." << std::endl;
